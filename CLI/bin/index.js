11039a7a49bdf9450c6dfc7b0b4c4cdc2783127c
kai stenbjorn <137458086+KaistenYT@users.noreply.github.com>
2026-01-10 21:47:16 -0400

    chore: prepare package for github registry publication

--- CLI/bin/index.js
+++ CLI/bin/index.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import { askQuestions } from "../lib/question.js";
 import { createStructure } from "../lib/structure.js";
 import { installDependencies } from "../lib/dependencies.js";
